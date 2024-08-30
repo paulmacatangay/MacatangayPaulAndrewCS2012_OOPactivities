3256c6f0f8ac69acf21eb679a41feadf3aa16259
paulmacatangay <165748980+paulmacatangay@users.noreply.github.com>
2024-08-30 14:12:52 +0800

    Add files via upload

---
+++ Laboratory Activities/Exercise Sets/Exercise Set 01/Tempconvert.java
@@ -0,0 +1,16 @@
+public class Escseq {
+    public static void main(String[] args) {
+
+        int multiple = 7;
+        int count = 5;
+
+        for (int i = 1; i <= count; i++) {
+            
+            if (i % 2 == 0) {
+                System.out.println("\t" + (multiple * i));
+            } else {
+                System.out.println(multiple * i);
+            }
+        }
+    }
+}
