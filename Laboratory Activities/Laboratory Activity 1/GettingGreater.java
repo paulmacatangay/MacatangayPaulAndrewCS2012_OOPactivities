2c34f2534d88e74a30de30f3ef8ede6d3471ba17
paulmacatangay <165748980+paulmacatangay@users.noreply.github.com>
2024-09-06 14:38:16 +0800

    Update GettingGreater.java

--- Laboratory Activities/Laboratory Activity 1/GettingGreater.java
+++ Laboratory Activities/Laboratory Activity 1/GettingGreater.java
@@ -14,9 +14,9 @@ class GettingGreater {
 
         char greaterChar = (char) Math.max(ch1, ch2);
 
-        System.out.println("--------------------------");
+        System.out.println("--------------------------------------------");
         System.out.println("The character with greater value is: " + greaterChar);
-        System.out.println("--------------------------");
+        System.out.println("--------------------------------------------");
         System.out.println("Showing the ASCII Codes");
         System.out.println(ch1 + " : " + (int) ch1);
         System.out.println(ch2 + " : " + (int) ch2);
